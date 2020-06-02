77e9b049aaa3ed04437759b2065d1ce9da01643c
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-06-02 22:26:40 +0900

    Create association

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,4 @@
 class Patient < ApplicationRecord
+  has_many :patient_users
+  has_many :users, through: :patient_users
 end
--- app/models/patient_user.rb
+++ app/models/patient_user.rb
@@ -1,2 +1,4 @@
 class PatientUser < ApplicationRecord
+  belongs_to :patient
+  belongs_to :users
 end
