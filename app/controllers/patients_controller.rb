132c484b3f062155c9b2b3d48ed52c6c792f23b1
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-06-06 23:18:15 +0900

    Preparation for show_action
    Show アクションの準備

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,4 +2,13 @@ class PatientsController < ApplicationController
   def index
     @patients = Patient.includes(:user).order("receipt_number DESC")
   end
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  private
+  def patient_params
+    params.require(:patient).permit(:name, :furigana, :birthday, :sex, :receipt_number, :insurance_number).merge(user_id: current_user.id)
+  end
 end
