67d851c1ca7522c603eb7a8b89b277c2f60bd719
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-06-14 20:24:01 +0900

    Create new, create controller

--- app/controllers/medication_histories_controller.rb
+++ app/controllers/medication_histories_controller.rb
@@ -1,2 +1,14 @@
 class MedicationHistoriesController < ApplicationController
+  def new
+    @medication_history = MedicationHistory.new
+  end
+
+  def create
+    MedicationHistory.create(medication_history_params)
+  end
+
+  private
+  def medication_history_params
+    params.require(:medication_history).permit(:subjective, :objective, :assessment, :education_plan, :care_plan, :observational_plan, :inquiry, :free_comment, :request_items, :change_condition, :medication_status, :side_effects, :allergies, :concomitant_drug, :otc, :complications, :medical_history, :constitution, :luxury_goods, :pregnancy_breastfeeding, :height_bodywight, :background,:medicine_notebook).merge(user_id: current_user.id, patient_id: params[:patient_id])
+  end
 end
